88f23021d3d0221be1b3ce34cae379b5964a5511
Kyle Walker <Kyle.Walker@zefr.com>
2013-10-26 19:02:19 -0700

    Restore correct version

--- back_end/blueprints/volunteer_venice.py
+++ back_end/blueprints/volunteer_venice.py
@@ -7,23 +7,49 @@ import traceback
 vv_blueprint = Blueprint('volunteer_venice', __name__)
 
 
+@vv_blueprint.route('/organization/<org_id>')
 @vv_blueprint.route('/organization')
 def organization(org_id=None):
-    '''First Test Route
+    '''
+    First Test Route
     '''
     result = dict()
 
     try:
-        query_data = g.db.query(Organization.id,
-                                Organization.name,
-                                Organization.address). \
-            order_by(Organization.id)
+        feilds = [Organization.id,
+                  Organization.name,
+                  Organization.website,
+                  Organization.address,
+                  Organization.description,
+                  Organization.category,
+                  Organization.phone_number,
+                  Organization.email,
+                  Organization.picture_location,
+                  Organization.video_location,
+                  ]
+
+        query_data = g.db.query(*feilds).order_by(Organization.id)
+
+        if org_id is not None:
+            query_data = query_data.filter(Organization.id == org_id)
 
         result['organization'] = []
         for x in query_data:
-            result['organization'].append({'id': x.id,
-                                           'name': x.name,
-                                           'address': x.address})
+            db_result = {'id': x.id,
+                         'name': x.name,
+                         'website': x.website,
+                         'address': x.address,
+                         'description': x.description,
+                         'category': x.category,
+                         'phone_number': x.phone_number,
+                         'email': x.email,
+                         'picture_location': x.picture_location,
+                         'video_location': x.video_location,
+                         }
+            if org_id is None:
+                result['organization'].append(db_result)
+            else:
+                result = db_result
 
     # Error occurred, add to message.
     except Exception as e:
